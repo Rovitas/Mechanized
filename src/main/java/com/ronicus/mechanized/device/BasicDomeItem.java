24d9be14aa8ee74fd057c23bc47d86d31c708c53
ronicus <2661052482@qq.com>
2026-01-21 20:53:24 +0800

    Add the startup animation of BasicDome.

--- src/main/java/com/ronicus/mechanized/device/BasicDomeItem.java
+++ src/main/java/com/ronicus/mechanized/device/BasicDomeItem.java
@@ -2,7 +2,13 @@ package com.ronicus.mechanized.device;
 
 import com.ronicus.mechanized.client.BasicDomeItemRenderer;
 import net.minecraft.client.renderer.BlockEntityWithoutLevelRenderer;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
 import net.minecraftforge.client.extensions.common.IClientItemExtensions;
 import software.bernie.geckolib.animatable.GeoItem;
 import software.bernie.geckolib.animatable.SingletonGeoAnimatable;
@@ -19,12 +25,12 @@ import java.util.function.Consumer;
 public class BasicDomeItem extends Item implements GeoItem {
     private static final RawAnimation IDLE = RawAnimation.begin().thenPlay("idle");
     private static final RawAnimation STARTUP = RawAnimation.begin().thenPlay("startup");
+    private static final RawAnimation STANDBY = RawAnimation.begin().thenPlay("standby");
     private final AnimatableInstanceCache cache = GeckoLibUtil.createInstanceCache(this);
     public BasicDomeItem(Properties properties) {
         super(properties);
         SingletonGeoAnimatable.registerSyncedAnimatable(this);
     }
-
 
     @Override
     public void initializeClient(Consumer<IClientItemExtensions> consumer) {
@@ -40,16 +46,27 @@ public class BasicDomeItem extends Item implements GeoItem {
             }
         });
     }
+
     private PlayState predicate(AnimationState state){
-        state.getController().setAnimation(IDLE);
+        state.getController().setAnimation(STANDBY);
         return PlayState.CONTINUE;
     }
+
     @Override
-    public void registerControllers(AnimatableManager.ControllerRegistrar controllerRegistrar) {
-        controllerRegistrar.add(new AnimationController<>(this, "controller", 0, this::predicate));
+    public void registerControllers(AnimatableManager.ControllerRegistrar data) {
+        data.add(new AnimationController<>(this,"standby_cr", 0, this::predicate));
+        data.add(new AnimationController<>(this,"startup_cr", state -> PlayState.CONTINUE).triggerableAnim("device_startup",STARTUP));
     }
+
     @Override
     public AnimatableInstanceCache getAnimatableInstanceCache() {
         return this.cache;
+    }
+
+    public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand){
+        if(level instanceof ServerLevel serverLevel){
+            this.triggerAnim(player, GeoItem.getOrAssignId(player.getItemInHand(hand), serverLevel), "startup_cr", "device_startup");
+        }
+        return super.use(level, player, hand);
     }
 }
