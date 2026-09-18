6f087e05b4a4eb4ac6c5947b502dd76f8632490e
ronicus <rollyear@qq.com>
2026-09-19 01:32:20 +0800

    chore: fix some bugs like misspellings due to the previous transplanting

--- src/main/java/com/ronicus/mechanized/ModItemGroups.java
+++ src/main/java/com/ronicus/mechanized/ModItemGroups.java
@@ -11,9 +11,9 @@ public class ModItemGroups {
     public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS =
             DeferredRegister.create(BuiltInRegistries.CREATIVE_MODE_TAB, Mechanized.MODID);
 
-    public static final DeferredHolder<CreativeModeTab, CreativeModeTab> MECHANIZED_TAB =
+    public static final DeferredHolder<CreativeModeTab, CreativeModeTab> MAIN =
             CREATIVE_MODE_TABS.register("mechanized_tab", () -> CreativeModeTab.builder()
-                    .title(Component.translatable("itemGroup.mechanized"))
+                    .title(Component.translatable("item_group.mechanized.main"))
                     .icon(() -> new ItemStack(ModItems.BASIC_DOME.get()))
                     .displayItems((params, output) -> {
                         output.accept(ModItems.BOOSTER.get());
--- src/main/java/com/ronicus/mechanized/client/BasicDomeItemModel.java
+++ src/main/java/com/ronicus/mechanized/client/BasicDomeItemModel.java
@@ -3,19 +3,20 @@ package com.ronicus.mechanized.client;
 import com.ronicus.mechanized.Mechanized;
 import com.ronicus.mechanized.device.BasicDomeItem;
 import net.minecraft.resources.ResourceLocation;
-import software.bernie.geckolib.model.GeoModel;
+import software.bernie.geckolib.model.DefaultedItemGeoModel;
 
-public class BasicDomeItemModel extends GeoModel<BasicDomeItem> {
+public class BasicDomeItemModel extends DefaultedItemGeoModel<BasicDomeItem> {
+    public BasicDomeItemModel() {
+        super(ResourceLocation.fromNamespaceAndPath(Mechanized.MODID, "basic_dome"));
+    }
+
     @Override
-    public ResourceLocation getModelResource(BasicDomeItem animatable) {
-        return ResourceLocation.fromNamespaceAndPath(Mechanized.MODID, "geo/basic_dome.geo.json");
+    public ResourceLocation buildFormattedModelPath(ResourceLocation basePath) {
+        return ResourceLocation.fromNamespaceAndPath(basePath.getNamespace(), "geo/" + basePath.getPath() + ".geo.json");
     }
 
-    public ResourceLocation getTextureResource(BasicDomeItem animatable) {
-        return ResourceLocation.fromNamespaceAndPath(Mechanized.MODID, "textures/item/basic_dome.png");
-    }
-
-    public ResourceLocation getAnimationResource(BasicDomeItem animatable) {
-        return ResourceLocation.fromNamespaceAndPath(Mechanized.MODID, "animations/basic_dome.animation.json");
+    @Override
+    public ResourceLocation buildFormattedAnimationPath(ResourceLocation basePath) {
+        return ResourceLocation.fromNamespaceAndPath(basePath.getNamespace(), "animations/" + basePath.getPath() + ".animation.json");
     }
 }
